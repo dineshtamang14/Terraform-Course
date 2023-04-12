4afcd57fa3fe7a8dd556b6c9dddf67399c4d5b70
Dinesh Tamang <dineshtamang7263@gmail.com>
2023-04-12 18:18:50 +0530

    Section 5 Completed

--- MetaArgument-for_each/c5-ec2instance.tf
+++ MetaArgument-for_each/c5-ec2instance.tf
@@ -1,11 +1,3 @@
-# Availability Zones DataSource
-data "aws_availability_zones" "my_azones" {
-  filter {
-    name = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
-
 # creating ec2 instance resource
 resource "aws_instance" "myec2vm" {
    ami = "${data.aws_ami.amzlinux2.id}"
@@ -15,7 +7,12 @@ resource "aws_instance" "myec2vm" {
    key_name = "${var.instance_keypair}"
 
    # Create EC2 Instance in all Availabilty Zones of a VPC
-   for_each = toset(data.aws_availability_zones.my_azones.names)
+  #  for_each = toset(data.aws_availability_zones.my_azones.names)
+   for_each = toset(keys({
+    for az, details in data.aws_ec2_instance_type_offerings.my_ins_type: 
+    az => details.instance_types if length(details.instance_types) != 0
+  }))
+  
    availability_zone = "${each.key}"
 
    # You can also use each.value because for list items each.key == each.value
