98e9bfa5a01fa9d26fc3c829a2fa581240a49001
Dinesh Tamang <dineshtamang7263@gmail.com>
2023-04-14 23:51:45 +0530

    Initiated AWS EC2 Bastion Host

---
+++ 07-AWS-EC2-BastionHost/c3-02-vpc-module.tf
@@ -0,0 +1,45 @@
+# Create VPC Terraform Module 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "4.0.1"
+  # version = "~> 4.0"
+
+  # VPC Basic Details
+  name = "${local.name}-${var.vpc_name}"
+  cidr = "${var.vpc_cidr_block}"
+  azs = "${var.vpc_availability_zones}"
+  private_subnets = "${var.vpc_private_subnets}"
+  public_subnets = "${var.vpc_public_subnets}"
+
+  # Database Subnets
+  database_subnets = "${var.vpc_database_subnets}"
+  create_database_subnet_group = "${var.vpc_create_database_subnet_group}"
+  create_database_subnet_route_table = "${var.vpc_create_database_subnet_route_table}"
+
+  # create_database_nat_gateway_route = true
+  # create_database_internet_gateway_route = false
+
+  # NAT Gateway - Outbound Communication
+  enable_nat_gateway = "${var.vpc_enable_nat_gateway}"
+  single_nat_gateway = "${var.vpc_single_nat_gateway}"
+
+  # VPC DNS Parameters
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  public_subnet_tags = {
+    Type = "public-subnets"
+  }
+
+  private_subnet_tags = {
+    Type = "private-subnets"
+  }
+
+  database_subnet_tags = {
+    Type = "database-subnets"
+  }
+
+  # Regular Tags
+  tags = "${local.common_tags}"
+  vpc_tags = "${local.common_tags}"
+}
