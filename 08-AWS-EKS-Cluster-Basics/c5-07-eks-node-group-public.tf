75a15722487a024623c0c63ca0873fc33ab30b1c
Dinesh Tamang <dineshtamang7263@gmail.com>
2023-04-21 19:40:12 +0530

    Created EKS Cluster and Nodes

---
+++ 08-AWS-EKS-Cluster-Basics/c5-07-eks-node-group-public.tf
@@ -0,0 +1,28 @@
+# Create AWS EKS Node Group - Public
+resource "aws_eks_node_group" "eks_ng_public" {
+  cluster_name = "${aws_eks_cluster.eks_cluster.name}"
+
+  node_group_name = "${local.name}-eks-ng-public"
+  node_role_arn = "${aws_iam_role.eks_nodegroup_role.arn}"
+  subent_ids = "${module.vpc.public_subnets}"
+# version = "${var.cluster_version}" # (Optional: Defaults to EKS Cluster Kubernetes version)
+
+  ami_type = "AL2_x86_64"
+  capacity_type = "ON_DEMAND"
+  disk_size = 20
+  instance_types = ["t2.small"]
+
+  remote_access {
+    ec2_ssh_key = "application-server-key"
+  }
+
+  scaling_config {
+    desired_size = 1
+    min_size = 1
+    max_size = 2
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+}
