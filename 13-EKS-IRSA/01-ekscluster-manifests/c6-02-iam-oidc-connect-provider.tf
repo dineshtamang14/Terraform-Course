8e854e1080ed4a9ec4bac15ba5c5b7336c2520ec
Dinesh Tamang <dineshtamang7263@gmail.com>
2023-05-18 23:00:35 +0530

    initial IRSA authentication and configuration done

---
+++ 13-EKS-IRSA/01-ekscluster-manifests/c6-02-iam-oidc-connect-provider.tf
@@ -0,0 +1,43 @@
+# Data Source: AWS Partition
+# Use this data source to lookup information about the current aws partition
+
+data "aws_partition" "current" {}
+
+# Resource: AWS IAM Open ID Connect Provider
+resource "aws_iam_openid_connect_provider" "oidc_provider" {
+  client_id_list  = ["sts.${data.aws_partition.current.dns_suffix}"]
+  thumbprint_list = ["${var.eks_oidc_root_ca_thumbprint}"]
+  url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+
+  # merge function will merge all object to single object
+  tags = merge(
+    {
+      Name = "${var.cluster_name}-eks-irsa"
+    },
+    local.common_tags
+  )
+}
+
+# Output: AWS IAM Open ID Connect Provider ARN
+output "aws_iam_openid_connect_provider_arn" {
+  description = "AWS IAM Open ID Connect Provider ARN"
+  value       = aws_iam_openid_connect_provider.oidc_provider.arn
+}
+
+# Extract OIDC Provider from OIDC Provider ARN
+locals {
+  aws_iam_oidc_connect_provider_extract_from_arn = element(split("oidc-provider/", "${aws_iam_openid_connect_provider.oidc_provider.arn}"), 1)
+}
+
+# Output: AWS IAM Open ID Connect Provider
+output "aws_iam_openid_connect_provider_extract_from_arn" {
+  description = "AWS IAM Open ID Connect Provider extract from ARN"
+  value       = local.aws_iam_oidc_connect_provider_extract_from_arn
+}
+
+# Sample Outputs for Reference
+/*
+aws_iam_openid_connect_provider_arn = "arn:aws:iam::180789647333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/A9DED4A4FA341C2A5D985A260650F232"
+aws_iam_openid_connect_provider_extract_from_arn = "oidc.eks.us-east-1.amazonaws.com/id/A9DED4A4FA341C2A5D985A260650F232"
+*/
+
