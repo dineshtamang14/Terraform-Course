98fe3885348a460080130a7c213fbdbd623e5458
Dinesh Tamang <dineshtamang7263@gmail.com>
2023-05-26 15:46:36 +0530

    Completed Section 18 Tasks

--- 18-EBS-CSI-Install-using-EKS-AddOn/02-ebs-addon-terraform-manifests/c1-versions.tf
+++ 18-EBS-CSI-Install-using-EKS-AddOn/02-ebs-addon-terraform-manifests/c1-versions.tf
@@ -1,3 +1,4 @@
+# Terraform Settings Block
 terraform {
   # terraform version
   # to allow all the terraform minor versions
@@ -9,27 +10,19 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.62" # optional but recommanded
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.9"
-    }
-    http = {
-      source  = "hashicorp/http"
-      version = "~> 3.3"
-    }
   }
-
-  # Adding Backend as s3 for Remote State Storage
+  # Adding Backend as S3 for Remote State Storage
   backend "s3" {
     bucket = "terraform-ekscluster-states"
-    key    = "dev/ebs-storage/terraform.tfstate"
+    key    = "dev/ebs-addon/terraform.tfstate"
     region = "us-east-1"
 
     # For State Locking
-    dynamodb_table = "dev-ebs-storage"
+    dynamodb_table = "dev-ebs-addon"
   }
 }
 
+# Terraform Provider Block
 provider "aws" {
   region = var.aws_region
 }
